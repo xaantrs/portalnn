0d2a9d41cfd7592fae81c415006d4b4ec74fc118
xaantrs <almeida.alexandret@gmail.com>
2025-10-24 12:12:57 -0300

    Mudança do init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,9 +10,7 @@ def create_app():
 
     # Importa e registra as rotas (Blueprints)
     from .routes.auth_routes import auth_bp
-    from .routes.geo_routes import geo_bp
 
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
-    app.register_blueprint(geo_bp, url_prefix='/api/geo')
 
     return app
